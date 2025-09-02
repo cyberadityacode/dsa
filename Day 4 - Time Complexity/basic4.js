26e57ef0260592ed27ec9f808961441bdcd4c3ef
cyberadityacode <adityadubey793@gmail.com>
2025-09-02 21:09:12 +0530

    basic revision

---
+++ Day 4 - Time Complexity/basic4.js
@@ -0,0 +1,22 @@
+const arrElement = [1, 2, 3, 4, 5];
+
+arrElement.forEach((e) => {
+    const f = e + 1;
+    console.log(f)
+});
+//Use forEach when you need side effects (logging, updating UI, calling an API, etc.).
+
+
+// Use map when you need to transform data into a new array.
+arrElement.map(n => console.log(n + 1));
+
+// cleaner and more modern ES6+ style with chaining.
+
+arrElement.map(e => e + 1) //transform each element
+    .forEach(e => console.log(e)); //log them
+
+// map returns the transformed array. forEach then logs each value.
+
+
+// Using modern features (spread + array methods)
+

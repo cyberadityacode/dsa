3d7bda27afd1b57afc708d20ca9c7b3ff27a6a00
cyberadityacode <adityadubey793@gmail.com>
2025-12-15 21:32:11 +0530

    convert linear time to constant

---
+++ Day 5 - DSA lesson 1/dsa-lesson-3.js
@@ -0,0 +1,13 @@
+// lets convert Linear Time O(n) complexity to Constant O(1)
+
+// for sum of elements
+
+/* 
+Mathematicians (like the young Carl Friedrich Gauss) 
+discovered a powerful formula for this calculation:
+*/
+function constantSum(numberOfElements) {
+  return (numberOfElements * (numberOfElements + 1)) / 2;
+}
+
+console.log(constantSum(10));

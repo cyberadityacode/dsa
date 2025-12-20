ced94af7790abbe2b15fe4f6cb37c7b5264353e3
cyberadityacode <adityadubey793@gmail.com>
2025-12-20 22:17:06 +0530

    challenge 1 learning

--- Day 7 - Frequency Counter ANAGRAM/challenge1.js
+++ Day 7 - Frequency Counter ANAGRAM/challenge1.js
@@ -13,3 +13,17 @@ same([1, 2, 3], [1, 9]) ⮕ false
 
 same([1, 2, 1], [4, 4, 1]) ⮕ false (must be the same frequency)
 */
+
+function sameArray(arrA, arrB) {
+  if (arrA.length !== arrB.length) {
+    return false;
+  }
+
+  const sqA = arrA.map((sq) => sq ** 2);
+  console.log("square of array A ", sqA);
+  console.log("whether its equal to B ", sqA == arrB);
+}
+
+console.log("Length of Array A", sameArray([1, 2, 3], [1, 4, 9]));
+
+console.info([1, 2, 3] == [1, 4, 9]); //condition will always return false, because JS compares Object by reference not by value

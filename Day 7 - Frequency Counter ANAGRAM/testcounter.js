129d0a9558fc444e7abd3bd5428ee113b643a88e
cyberadityacode <adityadubey793@gmail.com>
2025-12-20 20:02:08 +0530

    frequency counter function for string which return frequency object

---
+++ Day 7 - Frequency Counter ANAGRAM/testcounter.js
@@ -0,0 +1,23 @@
+function testString(str) {
+  if (typeof str !== "string" || str.length === 0) return false;
+
+  return true;
+}
+console.log("Testing empty string 1 ", testString(""));
+console.log("Testing string 2", testString("aditya"));
+console.log("Testing Number 3 ", testString(7));
+
+function testFrequncyCounter(str) {
+  if (!testString(str)) return false;
+  debugger;
+  const freq = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  return freq;
+}
+
+console.log("Function Passing string ", testFrequncyCounter("aditya"));
+console.log("Function Passing number ", testFrequncyCounter(7));

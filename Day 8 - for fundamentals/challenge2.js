8f1f8690d3e82e0b401d27926256e758ad645043
cyberadityacode <adityadubey793@gmail.com>
2026-01-01 10:07:21 +0530

    dsa(challenge2): sameFrequency code

--- Day 8 - for fundamentals/challenge2.js
+++ Day 8 - for fundamentals/challenge2.js
@@ -15,11 +15,73 @@ sameFrequency(34, 14) ⮕ false
 sameFrequency(3589578, 5879385) ⮕ true
 */
 
-function sameFrequencyTest(numOne,numTwo){
-    if( typeof numOne !== typeof numTwo) return false;
 
-    return true;
+/*
+Rejects: strings , NaN Infinity 
+*/
+/* function sameFrequencyTest(numOne,numTwo){
+    return Number.isFinite(numOne) && Number.isFinite(numTwo);
 }
 
 console.log(sameFrequencyTest(123,321));
 console.log(sameFrequencyTest(123,"321"));
+console.log(sameFrequencyTest("123","321"));
+
+ */
+
+function sameFrequency(numberOne,numberTwo){
+    // step 1 : validation
+    if(!Number.isFinite(numberOne) && Number.isFinite(numberTwo)){
+        return false;
+    }
+
+    // step 2: convert to strings
+
+    const str1 = String(numberOne);
+    const str2 = String(numberTwo);
+
+    // step 3: length check
+    if(str1.length !== str2.length){
+        return false;
+    }
+
+    // step 4: frequency counter
+    const freq = {};
+    for(let char of str1){
+        freq[char] = (freq[char] || 0) +1;
+    }
+
+    // step 5: compare
+    for(let char of str2){
+        if(!freq[char]){
+            return false;
+        }
+        freq[char]--;
+    }
+
+    return true;
+}
+
+console.log(sameFrequency(182, 281));       // true
+console.log(sameFrequency(34, 14));         // false
+console.log(sameFrequency(3589578, 5879385));// true
+
+console.log(sameFrequency(123, "321"));     // false
+console.log(sameFrequency("123", "321"));   // false
+console.log(sameFrequency(NaN, 123));       // false
+console.log(sameFrequency(12, Infinity));   // false
+
+
+
+// if order doesn't matter than its a piece of a cake.
+function isIdentical(num1,num2){
+    return num1.toString() === num2.toString();
+}
+
+console.log("is Identical ", isIdentical(182, 281));       // false
+console.log("is Identical ", isIdentical("182", "281"));       // false
+console.log("is Identical ", isIdentical(192, 129));       // false
+console.log("is Identical ", isIdentical(192, 192));       // true
+
+
+

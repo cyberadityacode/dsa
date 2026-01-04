152ca1d721fdfcb31843ca044dfc5ab7573372ab
cyberadityacode <adityadubey793@gmail.com>
2026-01-04 15:00:32 +0530

    dsa13- bubble sort optimized

--- Day 13- Bubble Sort/dsa13-bubbleSort.js
+++ Day 13- Bubble Sort/dsa13-bubbleSort.js
@@ -1,6 +1,7 @@
 let sampleArray = [3,14,2,4,11];
 
 // try to sort using bubble sort
+console.time();
 function bubbleSort(sampleArray){
     for(let i=sampleArray.length;i>0;i--){
         for(let j=0;j<i-1;j++){
@@ -12,5 +13,26 @@ function bubbleSort(sampleArray){
     }
     return sampleArray;
 }
+console.timeEnd();
 
 console.log(bubbleSort(sampleArray));
+
+// this will continue to loop even after sorting successfully until condition for 'i' is satisfied.
+console.time();
+function bubbleSortOptimized(sampleArray){
+    let swapped;
+    for(let i=sampleArray.length;i>0;i--){
+        swapped = false;
+
+        for(let j=0;j<i-1;j++){
+            if(sampleArray[j]>sampleArray[j+1]){
+                [sampleArray[j], sampleArray[j+1]] = [sampleArray[j+1],sampleArray[j]];
+                swapped= true;
+            }
+        }
+        if(!swapped) break;
+    }
+    return sampleArray;
+}
+console.timeEnd();
+console.log(bubbleSortOptimized(sampleArray));
